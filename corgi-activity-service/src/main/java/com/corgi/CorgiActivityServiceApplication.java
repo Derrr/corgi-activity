4b4a27681ba9cfeabc25703048e5a3332d138d29
tairanliu <dasltr@126.com>
2019-12-16 01:46:28 +0800

    init

--- corgi-activity-service/src/main/java/com/corgi/CorgiActivityServiceApplication.java
+++ corgi-activity-service/src/main/java/com/corgi/CorgiActivityServiceApplication.java
@@ -1,9 +1,11 @@
 package com.corgi;
 
+import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDubboConfiguration
 public class CorgiActivityServiceApplication {
 
 	public static void main(String[] args) {
