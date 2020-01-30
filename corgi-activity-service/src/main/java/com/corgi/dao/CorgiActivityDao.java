4ae5033852e35cdb7aac9ed7fd11c906470e2e8d
tairanliu <dasltr@126.com>
2020-01-30 20:25:01 +0800

    添加用户管理相关接口

--- corgi-activity-service/src/main/java/com/corgi/dao/CorgiActivityDao.java
+++ corgi-activity-service/src/main/java/com/corgi/dao/CorgiActivityDao.java
@@ -285,14 +285,14 @@ public class CorgiActivityDao {
     }
 
     public void removeActivity(String activityId) {
-        Query query = new Query(Criteria.where("_id").is(new ObjectId(activityId)));
+        Query query = new Query(Criteria.where("mongoId").is(new ObjectId(activityId)));
         corgiFavorActivityService.deleteByActivityId(activityId);
         mongoTemplate.remove(query, ActivityMongo.class);
     }
 
     public void updateActivityByColumn(String activityId, String column, String value) {
         Update update = new Update().set(column, value);
-        Query query = new Query(Criteria.where("_id").is(new ObjectId(activityId)));
+        Query query = new Query(Criteria.where("mongoId").is(new ObjectId(activityId)));
         mongoTemplate.updateFirst(query, update, ActivityMongo.class);
     }
 }
