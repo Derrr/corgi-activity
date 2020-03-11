115da9b5c8828f2d5fc343df037d33ca641b9d1a
tairanliu <dasltr@126.com>
2020-03-11 23:15:32 +0800

    add max threads

--- corgi-activity-service/src/main/java/com/corgi/dao/CorgiActivityDao.java
+++ corgi-activity-service/src/main/java/com/corgi/dao/CorgiActivityDao.java
@@ -113,7 +113,8 @@ public class CorgiActivityDao {
         }
         criteriaList.add(distanceCriteria);
         criteriaList.add(Criteria.where(ActivityMongo.SIGN_UP_TIME).gte(sdf.format(new Date())));
-        criteriaList.add(Criteria.where("status").ne(CorgiActivity.DELETED));
+        criteriaList.add(Criteria.where("status").is(CorgiActivity.CREATED));
+        
         if (!StringUtils.isEmpty(activityQuery.getType())) {
             criteriaList.add(Criteria.where("activityType").is(activityQuery.getType()));
         }
