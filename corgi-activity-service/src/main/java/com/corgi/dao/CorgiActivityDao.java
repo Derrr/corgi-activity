8f09e8edfa0f5e0b6e546ae45fd9ed5b478f8769
tairanliu <dasltr@126.com>
2020-03-11 22:32:03 +0800

    add max threads

--- corgi-activity-service/src/main/java/com/corgi/dao/CorgiActivityDao.java
+++ corgi-activity-service/src/main/java/com/corgi/dao/CorgiActivityDao.java
@@ -147,9 +147,6 @@ public class CorgiActivityDao {
 
         Criteria queryCriteria = new Criteria().andOperator(criteriaList.toArray(new Criteria[0]));
         Query query = new Query(queryCriteria).limit(100);
-        if (range <= 0) {
-            query = query.with(Sort.by(Sort.Direction.DESC, "mongoId"));
-        }
         List<ActivityMongo> corgiActivities = mongoTemplate.find(query, ActivityMongo.class);
 
         if (activityQuery.checkUser() && CollectionUtils.isNotEmpty(corgiActivities)) {
