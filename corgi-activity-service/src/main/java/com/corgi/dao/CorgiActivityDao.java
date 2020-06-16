a23ed7ed66e6ffd22d73f5b6bfea92c069820758
tairanliu <dasltr@126.com>
2020-06-16 18:18:39 +0800

    fix bug

--- corgi-activity-service/src/main/java/com/corgi/dao/CorgiActivityDao.java
+++ corgi-activity-service/src/main/java/com/corgi/dao/CorgiActivityDao.java
@@ -339,8 +339,7 @@ public class CorgiActivityDao {
     public long countActivity(String beginDate, String endDate) {
         Criteria criteria = new Criteria().andOperator(
                 Criteria.where("createTime").gte(beginDate),
-                Criteria.where("createTime").lte(endDate),
-                Criteria.where("category").is(CorgiActivity.CAT_ACTIVITY));
+                Criteria.where("createTime").lte(endDate));
         Query query = new Query(criteria);
         return mongoTemplate.count(query, ActivityMongo.class);
     }
