1909ea09a40790fcbb142158a1ff754994c17a10
tairanliu <dasltr@126.com>
2020-08-11 19:02:33 +0800

    fix bug

--- corgi-activity-service/src/main/java/com/corgi/dao/CorgiActivityDao.java
+++ corgi-activity-service/src/main/java/com/corgi/dao/CorgiActivityDao.java
@@ -223,9 +223,8 @@ public class CorgiActivityDao {
         if (CollectionUtils.isNotEmpty(activityQuery.getPayType())) {
             criteriaList.add(Criteria.where("payType").in(activityQuery.getPayType()));
         }
-        log.info("not city... {} ", activityQuery.getNotCity());
         if (!StringUtils.isEmpty(activityQuery.getNotCity())) {
-            criteriaList.add(Criteria.where("city").regex(activityQuery.getCity() + ".*").not());
+            criteriaList.add(Criteria.where("city").ne(activityQuery.getCity()));
         } else if (!StringUtils.isEmpty(activityQuery.getCity())) {
             criteriaList.add(Criteria.where("city").regex(activityQuery.getCity() + ".*"));
         }
