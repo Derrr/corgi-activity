db536ea378a12a58ed1434c7f86d5d120b2e02a7
tairanliu <dasltr@126.com>
2020-08-18 10:16:07 +0800

    添加推荐商户相关逻辑

--- corgi-activity-service/src/main/java/com/corgi/dao/CorgiActivityDao.java
+++ corgi-activity-service/src/main/java/com/corgi/dao/CorgiActivityDao.java
@@ -146,6 +146,7 @@ public class CorgiActivityDao {
         Criteria userCriteria = Criteria.where("userId").ne(activityQuery.getUserId());
         Criteria categoryCriteria = Criteria.where("category").is(activityQuery.getCategory());
         Criteria statusCriteria = Criteria.where("status").ne(CorgiActivity.DELETED);
+        Criteria checkCriteria = Criteria.where("checkStatus").is("pass");
         List<ActivityMongo> activityMongoList = new ArrayList<>();
 
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm");
@@ -168,7 +169,7 @@ public class CorgiActivityDao {
             Criteria startCriteria = Criteria.where("createTime").gte(startTime);
             Criteria endCriteria = Criteria.where("createTime").lte(endTime);
 
-            Criteria queryCriteria = new Criteria().andOperator(statusCriteria, distanceCriteria, startCriteria, endCriteria, userCriteria, categoryCriteria);
+            Criteria queryCriteria = new Criteria().andOperator(statusCriteria, distanceCriteria, startCriteria, endCriteria, userCriteria, categoryCriteria, checkCriteria);
             Query query = new Query(queryCriteria).with(Sort.by(Sort.Direction.DESC, "createTime"));
             List<ActivityMongo> corgiActivities = mongoTemplate.find(query, ActivityMongo.class);
             if (corgiActivities.size() > 0) {
@@ -258,9 +259,9 @@ public class CorgiActivityDao {
         if (activityQuery.getPageSize() != null && activityQuery.getPageSize() > 0) {
             size = activityQuery.getPageSize();
         }
-        if(activityQuery.getOffset() != null && activityQuery.getOffset() > 0){
-            skip=activityQuery.getOffset();
-        }else if (activityQuery.getPage() != null && activityQuery.getPage() > 0) {
+        if (activityQuery.getOffset() != null && activityQuery.getOffset() > 0) {
+            skip = activityQuery.getOffset();
+        } else if (activityQuery.getPage() != null && activityQuery.getPage() > 0) {
             skip = (activityQuery.getPage() - 1) * size;
         }
 
