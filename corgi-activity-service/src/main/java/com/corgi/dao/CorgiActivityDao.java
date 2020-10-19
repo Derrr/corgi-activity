b05bee03236bf8a576ab294a9bf7b42290db4055
tairanliu <dasltr@126.com>
2020-10-20 00:36:16 +0800

    fix bug

--- corgi-activity-service/src/main/java/com/corgi/dao/CorgiActivityDao.java
+++ corgi-activity-service/src/main/java/com/corgi/dao/CorgiActivityDao.java
@@ -292,14 +292,7 @@ public class CorgiActivityDao {
             criteriaList.add(Criteria.where(ActivityMongo.SIGN_UP_TIME).gte(new SimpleDateFormat("yyyy/MM/dd HH:mm").format(new Date())));
         }
         if (!StringUtils.isEmpty(activityQuery.getType())) {
-            if ("其他".equals(activityQuery.getType())) {
-                List<String> types = corgiToolService.getActivityTypes();
-                for (String type : types) {
-                    criteriaList.add(Criteria.where("activityType").ne(type));
-                }
-            } else {
-                criteriaList.add(Criteria.where("activityType").is(activityQuery.getType()));
-            }
+            criteriaList.add(Criteria.where("activityType").is(activityQuery.getType()));
         }
         if (CollectionUtils.isNotEmpty(activityQuery.getPayType())) {
             criteriaList.add(Criteria.where("payType").in(activityQuery.getPayType()));
