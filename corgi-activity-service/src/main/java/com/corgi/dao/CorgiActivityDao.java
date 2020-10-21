1df938fb934bb178a981efdf886d3a530e4e2695
tairanliu <dasltr@126.com>
2020-10-21 17:28:58 +0800

    修改动态列表

--- corgi-activity-service/src/main/java/com/corgi/dao/CorgiActivityDao.java
+++ corgi-activity-service/src/main/java/com/corgi/dao/CorgiActivityDao.java
@@ -293,7 +293,14 @@ public class CorgiActivityDao {
             criteriaList.add(Criteria.where(ActivityMongo.SIGN_UP_TIME).gte(new SimpleDateFormat("yyyy/MM/dd HH:mm").format(new Date())));
         }
         if (!StringUtils.isEmpty(activityQuery.getType())) {
-            criteriaList.add(Criteria.where("activityType").is(activityQuery.getType()));
+            if ("其他".equals(activityQuery.getType())) {
+                List<String> types = corgiToolService.getActivityTypes();
+                for (String type : types) {
+                    criteriaList.add(Criteria.where("activityType").ne(type));
+                }
+            } else {
+                criteriaList.add(Criteria.where("activityType").is(activityQuery.getType()));
+            }
         }
         if (CollectionUtils.isNotEmpty(activityQuery.getPayType())) {
             criteriaList.add(Criteria.where("payType").in(activityQuery.getPayType()));
@@ -325,6 +332,9 @@ public class CorgiActivityDao {
         }
         if (!StringUtils.isEmpty(activityQuery.getEndTime())) {
             criteriaList.add(Criteria.where(ActivityMongo.SIGN_UP_TIME).lte(activityQuery.getEndTime()));
+        }
+        if (!StringUtils.isEmpty(activityQuery.getTopic())) {
+            criteriaList.add(Criteria.where("topics").is(activityQuery.getTopic()));
         }
 
 
