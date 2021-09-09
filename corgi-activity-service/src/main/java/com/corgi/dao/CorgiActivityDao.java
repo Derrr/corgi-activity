32a8f75d1f7bdbadba4e696511d5c43d8f039c4b
liutairan <liu.tr@belle.com.cn>
2021-09-09 18:57:51 +0800

    fix bug

--- corgi-activity-service/src/main/java/com/corgi/dao/CorgiActivityDao.java
+++ corgi-activity-service/src/main/java/com/corgi/dao/CorgiActivityDao.java
@@ -625,7 +625,7 @@ public class CorgiActivityDao {
     public List<ActivityMongo> getActivityByFeed(String mongoId, Criteria criteria,Integer start, Integer size) {
         Query query = new Query().addCriteria(criteria).skip(start).limit(size).with(Sort.by(Sort.Direction.DESC, "mongoId"));
         if (!StringUtils.isEmpty(mongoId)) {
-            Criteria idCri = Criteria.where("_id").lt(new ObjectId(mongoId));
+            Criteria idCri = Criteria.where("mongoId").lt(mongoId);
             query.addCriteria(idCri);
         }
         return mongoTemplate.find(query, ActivityMongo.class);
