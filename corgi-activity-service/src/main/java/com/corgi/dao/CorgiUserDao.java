559557075772094306f6422271e6a7be8efaa120
trliu <trliu@hillinsight.com>
2023-06-19 16:20:35 +0800

    添加新排序

--- corgi-activity-service/src/main/java/com/corgi/dao/CorgiUserDao.java
+++ corgi-activity-service/src/main/java/com/corgi/dao/CorgiUserDao.java
@@ -83,10 +83,8 @@ public class CorgiUserDao {
         List<UserMongo> userMongos = mongoTemplate.find(query, UserMongo.class);
         UserDetail detail = corgiUserService.getUserDetailBasic(userQuery.getUserId());
         if (UserDetail.VERIFIED.equals(detail.getAvatarCheckStatus()) || "normal".equals(detail.getAvatarCheckStatus())) {
-            log.info("match has face...");
             return filterFace(userQuery, onlineMongos, userMongos);
         } else {
-            log.info("match no face...");
             return filterNoFace(userQuery, onlineMongos, userMongos);
         }
     }
@@ -349,7 +347,6 @@ public class CorgiUserDao {
             redisTemplate.delete(matchKey);
             redisTemplate.delete(viewKey);
         }
-        log.info("match type:{} size:{} remain:{}", filterType, result.size(), remain.size());
         return remain;
     }
 
