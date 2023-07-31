2590631f390cb7096c69251dc9aa97f5c0cc4a83
trliu <trliu@hillinsight.com>
2023-07-31 18:19:55 +0800

    添加新排序

--- corgi-activity-service/src/main/java/com/corgi/dao/CorgiUserDao.java
+++ corgi-activity-service/src/main/java/com/corgi/dao/CorgiUserDao.java
@@ -165,7 +165,7 @@ public class CorgiUserDao {
         if (query.getRange() == null || query.getRange() <= 0 || query.getRange() > 100) {
             query.setRange(1000.0);
         }
-        q.addCriteria(Criteria.where("location").nearSphere(new Point(query.getLng(), query.getLat())).maxDistance(query.getRange() / 111.12));
+        q.addCriteria(Criteria.where("location").nearSphere(new Point(query.getLng(), query.getLat())).maxDistance(query.getRange() / 6371.0));
         q.addCriteria(Criteria.where("userId").ne(query.getUserId()));
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
         if (!CollectionUtils.isEmpty(query.getDateStatus())) {
